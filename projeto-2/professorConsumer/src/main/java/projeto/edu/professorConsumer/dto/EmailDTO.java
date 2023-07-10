ff2b9f5c395f33f84553f01c11f2c9ae8a7c19c1
Dirceu Araújo Macêdo <dirceupwr@gmail.com>
2023-07-10 00:56:39 -0300

    projeto 2 - Ajustes DTOs

---
+++ projeto-2/professorConsumer/src/main/java/projeto/edu/professorConsumer/dto/EmailDTO.java
@@ -0,0 +1,14 @@
+package projeto.edu.professorConsumer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDTO {
+    private String emailTo;
+    private String subject;
+    private String text;
+}
